04aa2ffa434ea23917bc63907f97e2e41c8f44fc
Ivaylo Tabakov <ivotabakov1981@gmail.com>
2020-03-04 21:28:45 +0100

    init

---
+++ LegoApi/Models/LegoSet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LegoApi.Models
+{
+    public class LegoSet
+    {
+        public Guid Id { get; set; }
+
+        public int Year { get; set; }
+
+        public IEnumerable<string> KeyWords { get; set; }
+
+        public Uri Link { get; set; }
+    }
+}
