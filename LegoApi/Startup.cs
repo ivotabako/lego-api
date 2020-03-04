d2fd578d8a6d00412c441e7023ff2555ddb64de1
Ivaylo Tabakov <ivotabakov1981@gmail.com>
2020-03-04 21:49:46 +0100

    swagger added

--- LegoApi/Startup.cs
+++ LegoApi/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using LegoApi.Data;
+using Microsoft.OpenApi.Models;
 
 namespace LegoApi
 {
@@ -30,7 +31,13 @@ namespace LegoApi
             services.AddControllers();
 
             services.AddDbContext<LegoApiContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("LegoApiContext")));
+                    options.UseInMemoryDatabase("LegoInMemory"));
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +47,18 @@ namespace LegoApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseStaticFiles();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseHttpsRedirection();
 
