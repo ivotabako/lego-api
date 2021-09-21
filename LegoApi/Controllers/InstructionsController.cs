b8ce16d02a3e7af79573a4c9f1450c35d6b00ecb
ivotabako <ivotabakov1981@gmail.com>
2021-09-21 23:59:41 +0200

    added instruction controller

---
+++ LegoApi/Controllers/InstructionsController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LegoApi.Data;
+using LegoApi.Models;
+using Microsoft.Extensions.Logging;
+
+namespace LegoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InstructionsController : ControllerBase
+    {
+        private readonly LegoApiContext _context;
+        private readonly ILogger _logger;
+        public InstructionsController(LegoApiContext context, ILoggerFactory logger)
+        {
+            _context = context;
+
+            _logger = logger.CreateLogger(nameof(SetsController));
+        }
+
+        // GET: api/LegoSetInstructionsPages
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LegoSetInstructionsPage>>> GetLegoSetInstructionsPages()
+        {
+            return await _context.LegoSetInstructionsPages.ToListAsync();
+        }
+
+        // GET: api/LegoSetInstructionsPages/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<LegoSetInstructionsPage>> GetLegoSetInstructionsPage(Guid id)
+        {
+            var legoSetInstructionsPage = await _context.LegoSetInstructionsPages.FindAsync(id);
+
+            if (legoSetInstructionsPage == null)
+            {
+                _logger.LogInformation($"lego instructions with id {id} were not found");
+                return NotFound();
+            }
+
+            return legoSetInstructionsPage;
+        }
+
+        // PUT: api/LegoSetInstructionsPages/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLegoSetInstructionsPage(Guid id, LegoSetInstructionsPage legoSetInstructionsPage)
+        {
+            if (id != legoSetInstructionsPage.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(legoSetInstructionsPage).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LegoSetInstructionsPageExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/LegoSetInstructionsPages
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<LegoSetInstructionsPage>> PostLegoSetInstructionsPage(LegoSetInstructionsPage legoSetInstructionsPage)
+        {
+            _context.LegoSetInstructionsPages.Add(legoSetInstructionsPage);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLegoSetInstructionsPage", new { id = legoSetInstructionsPage.Id }, legoSetInstructionsPage);
+        }
+
+        // DELETE: api/LegoSetInstructionsPages/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<LegoSetInstructionsPage>> DeleteLegoSetInstructionsPage(Guid id)
+        {
+            var legoSetInstructionsPage = await _context.LegoSetInstructionsPages.FindAsync(id);
+            if (legoSetInstructionsPage == null)
+            {
+                return NotFound();
+            }
+
+            _context.LegoSetInstructionsPages.Remove(legoSetInstructionsPage);
+            await _context.SaveChangesAsync();
+
+            return legoSetInstructionsPage;
+        }
+
+        private bool LegoSetInstructionsPageExists(Guid id)
+        {
+            return _context.LegoSetInstructionsPages.Any(e => e.Id == id);
+        }
+    }
+}
